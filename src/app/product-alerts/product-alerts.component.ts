1ba5ff6d7cb345e8d7ebab96ca5b19713fae8926
liujuntao <ljt141421@aliyun.com>
2023-02-17 14:37:17 +0800

    父子组件通信

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -7,5 +7,8 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent {
+  //接收父组件（productListComponent）传递的数据
   @Input() product!: Product;
+  //创建一个事件触发器，为了与父组件（productListComponent）通信（传递参数或者触发事件）
+  @Output() notify = new EventEmitter();
 }
